e3d914d2a2263a3f3eb043b9d7376ae307db5609
Vinicius Campos <viniciuscampos4@gmail.com>
2025-05-18 20:47:37 -0300

    feat: include Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/quiz-logo.png';
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+};
+export default Header;
