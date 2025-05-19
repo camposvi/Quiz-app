7c4e3a5da1ce9e34a926b61121634a045af25696
Vinicius Campos <viniciuscampos4@gmail.com>
2025-05-18 21:43:37 -0300

    feat: include Quiz component  in main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Header from './components/Header/Header.jsx';
+import Quiz from './components/Quiz/Quiz.jsx';
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 };
 
 export default App;
